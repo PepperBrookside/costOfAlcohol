eb2fff66f76046eb33c7a451a616066c85235322
Zack Parr <zackparr@gmail.com>
2017-09-11 11:24:28 -0400

    Added an error catch

--- costofalcohol.py
+++ costofalcohol.py
@@ -6,15 +6,19 @@ def calculation (abv, oz, con, cost):
     return (3/abv) * (cost/oz)
 
 def operation():
-    abv = float(input("What is the ABV percentage? "))
-    oz = input("How many ounces are in a single container? Enter 'L' for litres. ")
-    if oz.lower() == 'l':
-        li = float(input("How many litres are in a single container? "))
-        oz = float(li * 33.8135)
-    else:
-        oz = float(oz)
-    con = int(input("How many containers are in the unit? "))
-    cost = float(input("How much is the unit? $"))
+    try:
+        abv = float(input("What is the ABV percentage? "))
+        oz = input("How many ounces are in a single container? Enter 'L' for litres. ")
+        if oz.lower() == 'l':
+            print("ml/1000 = litres")
+            li = float(input("How many litres are in a single container? "))
+            oz = float(li * 33.8135)
+        else:
+            oz = float(oz)
+        con = int(input("How many containers are in the unit? "))
+        cost = float(input("How much is the unit? $"))
+    except ValueError:
+        operation()
     total = calculation(abv, oz, con, cost)
     total = round(total, 2)
     print("3 ounces of pure alcohol from this unit costs ${}".format(float(total)))
